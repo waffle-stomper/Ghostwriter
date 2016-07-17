0bd46f345d867430c37323229ec954fe523add4d
waffle-stomper <waffle_stomper@live.com>
2016-07-17 14:04:47 +1000

    Revert "Update to 1.10.2"
    This reverts commit c6b6551c6d70f245d2a17cac8180b224407a8232.

--- forge/src/main/java/wafflestomper/ghostwriter/Ghostwriter.java
+++ forge/src/main/java/wafflestomper/ghostwriter/Ghostwriter.java
@@ -24,7 +24,7 @@ import net.minecraftforge.fml.common.gameevent.TickEvent.RenderTickEvent;
 public class Ghostwriter{
 	
     public static final String MODID = "Ghostwriter";
-    public static final String VERSION = "1.10.2-1.8.1";
+    public static final String VERSION = "1.10.0-1.8.0";
     public static final String NAME = "Ghostwriter";
 	
 	private Minecraft mc = Minecraft.getMinecraft();
