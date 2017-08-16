6ce1c2b3f8c3cd15c0df58652c4dec78b708d885
waffle-stomper <waffle_stomper@live.com>
2017-08-16 11:53:26 +1000

    Prevent 'Book edited too quickly' error

--- forge/src/main/java/wafflestomper/ghostwriter/Ghostwriter.java
+++ forge/src/main/java/wafflestomper/ghostwriter/Ghostwriter.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class Ghostwriter{
 	
     public static final String MODID = "ghostwriter";
-    public static final String VERSION = "1.9.1";
+    public static final String VERSION = "1.9.7";
     public static final String NAME = "Ghostwriter";
 	
 	private Minecraft mc = Minecraft.getMinecraft();
