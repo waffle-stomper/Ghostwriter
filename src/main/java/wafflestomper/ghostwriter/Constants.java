a1cbe98b5dddb955f3cc5d2fdde3e1e2ccde866e
waffle-stomper <waffle_stomper@live.com>
2021-02-16 08:24:39 +1000

    Add Constants class

---
+++ src/main/java/wafflestomper/ghostwriter/Constants.java
@@ -0,0 +1,7 @@
+package wafflestomper.ghostwriter;
+
+public class Constants {
+	// TODO: Move other constants here
+	// Maximum length allowed before the book is considered invalid by the client (as of 1.16.1)
+	public static final int BOOK_TITLE_MAX_LEN = 32;
+}
