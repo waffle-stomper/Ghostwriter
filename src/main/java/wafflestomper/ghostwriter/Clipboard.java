644731a1b5a13cfb880c03355f0db73d8ee179a7
waffle-stomper <waffle_stomper@live.com>
2021-02-19 14:21:24 +1000

    Formatting

--- src/main/java/wafflestomper/ghostwriter/Clipboard.java
+++ src/main/java/wafflestomper/ghostwriter/Clipboard.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Clipboard {
+	
 	public final List<String> pages = new ArrayList<>();
-	/**
-	 * Used for copying one or more pages (so we don't have to wipe out a book in the clipboard)
-	 */
+	//Used for copying one or more pages (so we don't have to wipe out a book in the clipboard)
 	public final List<String> miscPages = new ArrayList<>();
 	//Stores entire books
 	public String title = "";
 	public String author = "";  // Even though we can't set it when signing, this is still read from signed books
 	public boolean bookInClipboard = false;
+	
 	
 	public void clearBook() {
 		this.title = "";
